353e0e01eaecf9c38f7c6e3c0cc1f1d193e47186
Yevhen Shved <Dok6857@gmail.com>
2024-08-16 23:16:38 +0300

    save

--- test-task/src/app/app.component.ts
+++ test-task/src/app/app.component.ts
@@ -1,14 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  // template: ``,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'test-task';
+  password: string = '';
+  isPasswordVisible = false;
+
+  onToggleVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+  
+  onInput(event: Event) {
+   const input = event.target as HTMLInputElement;
+   const passwordEasy = document.getElementById('p-easy');
+   const passwordMedium = document.getElementById('p-medium');
+   const passwordStrong= document.getElementById(' p-strong');
+   if (input.value.length < 8) {
+    
+   }
+   console.log(input.value);
+  }
 }
