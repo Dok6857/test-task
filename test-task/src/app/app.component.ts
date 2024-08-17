6234df7a4739e3e0afb63bec47772d215e5fada4
Yevhen Shved <Dok6857@gmail.com>
2024-08-17 14:32:16 +0300

    add tests and commonModule

--- test-task/src/app/app.component.ts
+++ test-task/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -5,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -16,10 +17,15 @@ export class AppComponent {
   onToggleVisibility() {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
-  
+
   onInput(event: Event) {
-   const input = event.target as HTMLInputElement;
-  //  const hasLetters = /[a-zA-Z]/.test(input)
-   console.log(this.password.length);
+    const input = event.target as HTMLInputElement;
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasNumbers = /[0-9]/.test(this.password);
+    const hasSymbols = /[^a-zA-Z0-9]/.test(this.password);
+
+    
+
+    console.log(this.password.length);
   }
 }
